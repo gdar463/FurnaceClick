9597c5c52cee6f9dc9e5d357a620a200355d880d
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-12-28 23:56:38 +0100

    Ahia, iaia, iai cantava

--- src/main/java/com/gdar463/furnaceclick/FurnaceClick.java
+++ src/main/java/com/gdar463/furnaceclick/FurnaceClick.java
@@ -5,10 +5,10 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.registry.Registries;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import java.util.Arrays;
 
@@ -19,7 +19,7 @@ public class FurnaceClick implements ModInitializer {
 	public void onInitialize() {
 		AttackBlockCallback.EVENT.register(((player, world, hand, pos, direction) -> {
 			BlockEntity block = world.getBlockEntity(pos);
-			if (Arrays.asList(FurnaceClick.allowedBlocks).contains(Registry.BLOCK.getId(world.getBlockState(pos).getBlock())) && direction == world.getBlockState(pos).get(Properties.HORIZONTAL_FACING)) {
+			if (Arrays.asList(FurnaceClick.allowedBlocks).contains(Registries.BLOCK.getId(world.getBlockState(pos).getBlock())) && direction == world.getBlockState(pos).get(Properties.HORIZONTAL_FACING)) {
 				Inventory furnaceInventory = (Inventory) block;
                 if (furnaceInventory != null && furnaceInventory.getStack(2).getCount() > 0) {
 					player.getInventory().offerOrDrop(furnaceInventory.removeStack(2));
