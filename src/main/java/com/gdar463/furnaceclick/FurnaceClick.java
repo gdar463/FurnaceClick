c84495d07d54df2130a06f0394911335c8d3fb1f
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-12-28 23:53:29 +0100

    Quilt Stuff

--- src/main/java/com/gdar463/furnaceclick/FurnaceClick.java
+++ src/main/java/com/gdar463/furnaceclick/FurnaceClick.java
@@ -1,7 +1,5 @@
 package com.gdar463.furnaceclick;
 
-import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.inventory.Inventory;
@@ -9,6 +7,8 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 import java.util.Arrays;
 
@@ -16,16 +16,16 @@ public class FurnaceClick implements ModInitializer {
 	public static Identifier[] allowedBlocks = {new Identifier("minecraft", "furnace"), new Identifier("minecraft", "smoker"), new Identifier("minecraft", "blast_furnace")};
 
 	@Override
-	public void onInitialize() {
-		AttackBlockCallback.EVENT.register(((player, world, hand, pos, direction) -> {
+	public void onInitialize(ModContainer mod) {
+		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
 			BlockEntity block = world.getBlockEntity(pos);
 			if (Arrays.asList(FurnaceClick.allowedBlocks).contains(Registry.BLOCK.getId(world.getBlockState(pos).getBlock())) && direction == world.getBlockState(pos).get(Properties.HORIZONTAL_FACING)) {
 				Inventory furnaceInventory = (Inventory) block;
-                if (furnaceInventory != null && furnaceInventory.getStack(2).getCount() > 0) {
+				if (furnaceInventory != null && furnaceInventory.getStack(2).getCount() > 0) {
 					player.getInventory().offerOrDrop(furnaceInventory.removeStack(2));
-                }
-            }
+				}
+			}
 			return ActionResult.PASS;
-		}));
+		});
 	}
 }
